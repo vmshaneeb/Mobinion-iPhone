39317da7651fc0c310cd3222bece8bb5f97b5051
Mohammed Shaneeb <vmshaneeb@yahoo.com>
2016-04-05 22:29:29 +0300

    additional files outside the original folder

---
+++ Mobinion/InterestsCollectionReusableView.swift
@@ -0,0 +1,22 @@
+//
+//  InterestsCollectionReusableView.swift
+//  Mobinion
+//
+//  Created by Mohammed Shaneeb on 3/29/16.
+//  Copyright © 2016 Novateurglow. All rights reserved.
+//
+
+import UIKit
+
+class InterestsCollectionReusableView: UICollectionReusableView
+{
+
+    @IBOutlet weak var headerLabel: UILabel!
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
