1194a1398280a438ababe02b12f8c63fad739351
vmshaneeb <vmshaneeb@gmail.com>
2016-04-28 00:20:16 +0300

    Installing PhoneNumberKit (0.7)

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -88,6 +88,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Cloudinary.framework"
   install_framework "Pods/DBAlertController.framework"
   install_framework "Pods/IQKeyboardManager.framework"
+  install_framework "Pods/PhoneNumberKit.framework"
   install_framework "Pods/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -95,5 +96,6 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Cloudinary.framework"
   install_framework "Pods/DBAlertController.framework"
   install_framework "Pods/IQKeyboardManager.framework"
+  install_framework "Pods/PhoneNumberKit.framework"
   install_framework "Pods/SwiftyJSON.framework"
 fi
