db7b159a68f7f042daee60fdcc582c5a8e87875f
vmshaneeb <vmshaneeb@gmail.com>
2016-04-29 04:41:42 +0300

    tabbar UI changes….

---
+++ Mobinion/ManualCode.swift
@@ -0,0 +1,27 @@
+//
+//  ManualCode.swift
+//  Mobinion
+//
+//  Created by Mohammed Shaneeb on 4/29/16.
+//  Copyright © 2016 Novateurglow. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import DBAlertController
+
+class ManualCode: UIViewController
+{
+    override func viewDidAppear(animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
