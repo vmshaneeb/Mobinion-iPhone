e9d5663c6e406bf06284d5a485ce4891d751f8f6
vmshaneeb <vmshaneeb@gmail.com>
2016-05-26 04:27:35 +0300

    - new string extension

--- Mobinion/Extensions.swift
+++ Mobinion/Extensions.swift
@@ -70,9 +70,12 @@ extension UIImage
             
             if CGImageDestinationFinalize(destination!)
             {
+                return true
+            }
+            else
+            {
                 return false
             }
-            return true
         }
         return true
     }
@@ -167,4 +170,56 @@ extension UIColor
     
 }
 
+extension String {
+    
+    var lastPathComponent: String
+        {
+        get
+        {
+            return (self as NSString).lastPathComponent
+        }
+    }
+    
+    var pathExtension: String {
+        get {
+            
+            return (self as NSString).pathExtension
+        }
+    }
+    
+    var stringByDeletingLastPathComponent: String {
+        get {
+            
+            return (self as NSString).stringByDeletingLastPathComponent
+        }
+    }
+    
+    var stringByDeletingPathExtension: String {
+        get {
+            
+            return (self as NSString).stringByDeletingPathExtension
+        }
+    }
+    
+    var pathComponents: [String] {
+        get {
+            
+            return (self as NSString).pathComponents
+        }
+    }
+    
+    func stringByAppendingPathComponent(path: String) -> String {
+        
+        let nsSt = self as NSString
+        
+        return nsSt.stringByAppendingPathComponent(path)
+    }
+    
+    func stringByAppendingPathExtension(ext: String) -> String? {
+        
+        let nsSt = self as NSString
+        
+        return nsSt.stringByAppendingPathExtension(ext)
+    }
+}
 
