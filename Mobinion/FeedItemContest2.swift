659fe0f095985192e4f0acbfae19862dfb6d8514
vmshaneeb <vmshaneeb@gmail.com>
2016-05-31 05:19:04 +0300

    - controller files for the new views

---
+++ Mobinion/FeedItemContest2.swift
@@ -0,0 +1,24 @@
+//
+//  FeedItemContest2.swift
+//  Mobinion
+//
+//  Created by Mohammed Shaneeb on 5/31/16.
+//  Copyright © 2016 Novateurglow. All rights reserved.
+//
+
+import UIKit
+
+class FeedItemContest2: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ Mobinion/FeedItemCurrentStand.swift
@@ -0,0 +1,24 @@
+//
+//  FeedItemCurrentStand.swift
+//  Mobinion
+//
+//  Created by Mohammed Shaneeb on 5/31/16.
+//  Copyright © 2016 Novateurglow. All rights reserved.
+//
+
+import UIKit
+
+class FeedItemCurrentStand: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
