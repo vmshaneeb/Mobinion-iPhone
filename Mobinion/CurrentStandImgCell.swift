f2fd27828d30b52cea9f1d5260746ca8425794dd
vmshaneeb <vmshaneeb@gmail.com>
2016-06-02 23:18:14 +0300

    - CurrentStandImg Cell

---
+++ Mobinion/CurrentStandImgCell.swift
@@ -0,0 +1,27 @@
+//
+//  CurrentStandImgCell.swift
+//  Mobinion
+//
+//  Created by Mohammed Shaneeb on 6/2/16.
+//  Copyright © 2016 Novateurglow. All rights reserved.
+//
+
+import UIKit
+
+class CurrentStandImgCell: UITableViewCell
+{
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
