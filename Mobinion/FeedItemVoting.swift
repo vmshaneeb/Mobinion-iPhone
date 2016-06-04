a440bbe996bc5581ffb60c6e1d6a92af7ef9d8e9
vmshaneeb <vmshaneeb@gmail.com>
2016-06-04 06:25:24 +0300

    - FeedItemVoting actions n custom functions inc. API calls

--- Mobinion/FeedItemVoting.swift
+++ Mobinion/FeedItemVoting.swift
@@ -36,10 +36,10 @@ class FeedItemVoting: UIViewController
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print(itemType)
+//        print(itemType)
         if itemType == "mark"
         {
-            print(submitBtn.titleLabel?.text)
+//            print(submitBtn.titleLabel?.text)
             submitBtn.titleLabel?.text = "SUBMIT YOUR MARKS"
         }
     }
@@ -61,5 +61,128 @@ class FeedItemVoting: UIViewController
     {
         
     }
+    
+    //MARK:- Custom Functions
+    func doalertView (tit: String, msgs: String)
+    {
+        let titles = tit.capitalizedString
+        let messages = msgs.capitalizedString
+        let alertController = UIAlertController(title: titles, message: messages, preferredStyle: .Alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        
+        alertController.addAction(defaultAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func doDBalertView (tit: String, msgs: String)
+    {
+        let titles = tit.capitalizedString
+        let messages = msgs.capitalizedString
+        let alertController = DBAlertController(title: titles, message: messages, preferredStyle: .Alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        
+        alertController.addAction(defaultAction)
+        alertController.show()
+    }
+    
+    func submitVoteAPI(completionHandler : (NSDictionary?, NSData?, NSError?) -> Void)
+    {
+        let toks = NSUserDefaults.standardUserDefaults().objectForKey("token")
+        //print(toks)
+        
+        let header = ["Authorization": toks as! String]
+        //print(header)
+        
+        let URL = "http://vyooha.cloudapp.net:1337/submitVote"
+        
+        var parameter = [String: AnyObject]()
+        
+        if itemType == "mark"
+        {
+            parameter = ["voteId": itemID,
+                         "answers": ""]
+        }
+        else
+        {
+            parameter = ["voteId": itemID,
+                         "answers": ""]
+        }
+        
+        Alamofire.request(.POST, URL, headers: header, parameters: parameter, encoding: .JSON)
+            .responseJSON { response in
+                switch response.result
+                {
+                case .Success:
+                    if let value = response.result.value
+                    {
+                        completionHandler(value as? NSDictionary, response.data, nil)
+                    }
+                    
+                case .Failure(let error):
+                    completionHandler(nil, nil, error)
+                }
+        }
+    }
+    
+    func addCommentDetails()
+    {
+        self.StartLoader()
+        
+        submitVoteAPI()
+        { value, data, error in
+            if value != nil
+            {
+                let json = JSON(value!)
+                print(json)
+                
+                self.HideLoader()
+                
+                let titles = json["status"].stringValue
+                let messages = json["message"].stringValue
+                
+                if titles == "error"
+                {
+                    self.doDBalertView(titles, msgs: messages)
+                    print(messages)
+                }
+                else
+                {
+                    //                    do
+                    //                    {
+                    //
+                    //                    }
+                    //                    catch
+                    //                    {
+                    //                        print("error in responseObject")
+                    //                    }
+                }
+            }
+            else
+            {
+                self.HideLoader()
+                //                    print(error)
+                self.doDBalertView("Error", msgs: (error?.localizedDescription)!)
+            }
+    }
+    }
+    
+    // MARK: - Loader
+    func StartLoader()
+    {
+        let objOfHUD:MBProgressHUD=MBProgressHUD .showHUDAddedTo(self.view, animated: true)
+        objOfHUD.labelText="Loading.."
+    }
+    
+    func stopLoader()
+    {
+        MBProgressHUD.hideHUDForView(self.view, animated: true)
+    }
+    
+    func HideLoader()
+    {
+        self.stopLoader()
+    }
+
 }
 
